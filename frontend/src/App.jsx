830339eb5adbadbcc5792b6c37df2a25c9a82068
yism97 <yihs97@naver.com>
2026-05-18 14:14:17 +0900

    feat: add frontend pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,59 @@ import "./App.css";
 import Navbar from "./Components/Navbar/Navbar";
 import Footer from "./Components/Footer/Footer";
 
-function App() {
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+
+import Main from "./Page/Main/Main";
+import About from "./Page/About/About";
+import LeaderShip from "./Page/LeaderShip/LeaderShip";
+import Board from "./Page/Board/Board";
+import Services from "./Page/Services/Services";
+import Contact from "./Page/Contact/Contact";
+
+function Layout() {
   return (
-    <div>
+    <>
       <Navbar />
+      <Outlet />
       <Footer />
-    </div>
+    </>
   );
+}
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <Main />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+      {
+        path: "leadership",
+        element: <LeaderShip />,
+      },
+      {
+        path: "board",
+        element: <Board />,
+      },
+      {
+        path: "our-services",
+        element: <Services />,
+      },
+      {
+        path: "contact",
+        element: <Contact />,
+      },
+    ],
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ frontend/src/Page/LeaderShip/LeaderShip.jsx
@@ -0,0 +1,5 @@
+const LeaderShip = () => {
+  return <div>LeaderShip</div>;
+};
+
+export default LeaderShip;
